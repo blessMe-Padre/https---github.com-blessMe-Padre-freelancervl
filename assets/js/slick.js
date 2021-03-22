76b2db946cd41ec9dd76edd523ecac108817696f
Sergey Subbotin <zarodiny@yandex.ru>
2021-03-22 18:43:56 +1000

    Реализовал кнопку вверх, разбил js на разные файлы

---
+++ assets/js/slick.js
@@ -0,0 +1,27 @@
+$(function () {
+    $('.top__slider').slick({
+        arrows: false,
+        dots: true,
+        autoplay: true,
+        fade: true,
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    dots: false
+                }
+            },
+        ]
+    });
+
+});
+
+
+$(function () {
+    $('.fairy__slider').slick({
+        prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="assets/images/fairy/vector_left.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-btn slick-next"><img src="assets/images/fairy/vector_right.svg" alt=""></button>',
+        autoplay: true,
+        responsive: [{ breakpoint: 601, settings: { arrows: false } },]
+    });
+});
