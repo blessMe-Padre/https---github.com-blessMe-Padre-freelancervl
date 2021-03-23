ba09fbc27dca888601d6cf19d8c92513f63f1095
Sergey Subbotin <zarodiny@yandex.ru>
2021-03-23 14:19:43 +1000

    довавлен блок старт и анимация

---
+++ assets/js/animation.js
@@ -0,0 +1,53 @@
+
+//получаем в переменную animItems все обьекты к которым применяется
+//анимация (класс ._anim-items добавлять вручную в html к элементу который должен быть анимирован)
+// код добавляет к объекту класс "_active" свойства анимации редактируются в css
+// класс _anim-no-hide отключает повторную анимацию при скроле вверх страницы
+// пример 
+//  .start__title {
+//     transform: translate(0px, 120%);
+//     opacity: 0;
+//     transition: all 0.8s ease 0s;
+// }
+// .start__title._active {
+//     transform: translate(0px, 0);
+//     opacity: 1;  
+// }
+
+
+const animItems = document.querySelectorAll('._anim-items');
+
+if (animItems.length > 0) {
+    window.addEventListener('scroll', animOnScroll);
+    function animOnScroll() {
+        for (let index = 0; index < animItems.length; index++) {
+            const animItem = animItems[index];
+            const animItemHeight = animItem.offsetHeight;
+            const animItemOffset = offset(animItem).top;
+            const animStart = 4;
+
+            let animItemPoint = window.innerHeight - animItemHeight / animStart;
+            if (animItemHeight > window.innerHeight) {
+                animItemPoint = window.innerHeight - window.innerHeight / animStart;
+            }
+
+            if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+                animItem.classList.add('_active');
+            } else {
+                if (!animItem.classList.contains('_anim-no-hide')) {
+                    animItem.classList.remove('_active');
+                }
+            }
+        }
+    }
+    function offset(el) {
+        const rect = el.getBoundingClientRect(),
+            scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+            scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+    }
+
+    setTimeout(() => {
+        animOnScroll();
+    }, 300);
+}
