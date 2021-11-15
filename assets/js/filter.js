384e4b575d8ccdda5a4b7e25e030ac790176bbf6
Сергей Субботин <zarodiny@yandex.ru>
2021-11-15 14:28:40 +1000

    удаление блока галереи. Создание шаблона порфолио

--- assets/js/filter.js
+++ assets/js/filter.js
@@ -1,6 +1,6 @@
 function app() {
-    let buttons = document.querySelectorAll('.gallery__btn')
-    let cards = document.querySelectorAll('.gallery__item')
+    let buttons = document.querySelectorAll('.gallery__btn');
+    let cards = document.querySelectorAll('.gallery__item');
 
     function filter(category, items) {
         items.forEach((item) => {
@@ -11,6 +11,7 @@ function app() {
             } else {
                 item.classList.remove('gallery_hide')
             }
+
 
         })
     }
