7a53b938635938e50aac109aedbec26f8d8f52e8
Сергей Субботин <zarodiny@yandex.ru>
2021-11-23 20:04:36 +1000

    переписал up на чистом js

--- assets/js/up.js
+++ assets/js/up.js
@@ -1,20 +1,35 @@
-//кнопка прокрутки вврерх 
+//кнопка прокрутки вврерх
 
-function backToTop() {
-    let button = $('.back-to-top');
+//Jquery
+// function backToTop() {
+//     let button = $('.back-to-top');
 
-    $(window).on('scroll', () => {
-        if ($(this).scrollTop() >= 50) {
-            button.fadeIn();
-        } else {
-            button.fadeOut();
-        }
-    });
+//     $(window).on('scroll', () => {
+//         if ($(this).scrollTop() >= 50) {
+//             button.fadeIn();
+//         } else {
+//             button.fadeOut();
+//         }
+//     });
 
-    button.on('click', (e) => {
-        e.preventDefault();
-        $('html').animate({ scrollTop: 0 }, 1000);
-    })
-}
+//     button.on('click', (e) => {
+//         e.preventDefault();
+//         $('html').animate({ scrollTop: 0 }, 1000);
+//     })
+// }
 
-backToTop();
+// backToTop();
+
+//JS
+const scrollBtn = document.querySelector('.back-to-top');
+window.onscroll = () => {
+    if (window.scrollY > 600) {
+        scrollBtn.classList.remove('back-to-top--hide');
+    } else if (window.scrollY < 700) {
+        scrollBtn.classList.add('back-to-top--hide');
+    }
+};
+scrollBtn.onclick = (e) => {
+    e.preventDefault();
+    window.scrollTo(0, 0);
+};
