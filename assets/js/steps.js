bde2f4b5480b8d2efb02fdb6a7cb13e980b05358
Сергей Субботин <zarodiny@yandex.ru>
2022-06-02 16:39:35 +1000

    добавляет стили и рефакторинг кода в steps

--- assets/js/steps.js
+++ assets/js/steps.js
@@ -1,52 +1,53 @@
 const stepSection = document.querySelector('.steps');
-const stepNumbers = document.querySelectorAll('.step__number');
-const progressSuccess = document.querySelector('.steps__progress-success');
-const stepItems = document.querySelectorAll('.steps__item');
-const prevButton = document.querySelector('.button--prev');
-const nextButton = document.querySelector('.button--next');
 
-function getSteps() {
-    let step = 0;
+if (stepSection) {
+    function getSteps() {
+        const stepNumbers = document.querySelectorAll('.step__number');
+        const progressSuccess = document.querySelector('.steps__progress-success');
+        const stepItems = document.querySelectorAll('.steps__item');
+        const prevButton = document.querySelector('.step__button--prev');
+        const nextButton = document.querySelector('.step__button--next');
 
-    prevButton.addEventListener('click', () => {
-        step--;
-        stepNumbers[step + 1].classList.remove('step__number--active');
-        updateSteps();
-    })
+        let step = 0;
 
-    nextButton.addEventListener('click', () => {
-        step++;
-        updateSteps();
-    })
-
-    function updateSteps() {
-        stepItems.forEach(item => {
-            item.classList.contains('active') && item.classList.remove('active');
+        prevButton.addEventListener('click', () => {
+            step--;
+            stepNumbers[step + 1].classList.remove('step__number--active');
+            updateSteps();
         })
 
-        stepItems[step].classList.add('active');
-        stepNumbers[step].classList.add('step__number--active');
+        nextButton.addEventListener('click', () => {
+            step++;
+            updateSteps();
+        })
 
-        if (step === 0) {
-            prevButton.style.display = "none";
-        } else {
-            prevButton.style.display = "inline-block";
+        function updateSteps() {
+            stepItems.forEach(item => {
+                item.classList.contains('active') && item.classList.remove('active');
+            })
+
+            stepItems[step].classList.add('active');
+            stepNumbers[step].classList.add('step__number--active');
+
+            if (step === 0) {
+                prevButton.style.display = "none";
+            } else {
+                prevButton.style.display = "inline-block";
+            }
+
+            if (step === stepItems.length - 1) {
+                nextButton.style.display = "none";
+            } else {
+                nextButton.style.display = "inline-block";
+            }
+
+            // закрашивает активный прогресбар (задает ширину, через стили добавляем цвет)
+            const stepNumberActives = document.querySelectorAll('.step__number--active');
+            const percent = ((stepNumberActives.length - 1) / (stepNumbers.length - 1)) * 100 + '%';
+            progressSuccess.style.width = percent;
         }
-
-        if (step === stepItems.length - 1) {
-            nextButton.style.display = "none";
-        } else {
-            nextButton.style.display = "inline-block";
-        }
-
-        // закрашивает активный прогресбар (задает ширину, через стили добавляем цвет)
-        const stepNumberActives = document.querySelectorAll('.step__number--active');
-        const percent = ((stepNumberActives.length - 1) / (stepNumbers.length - 1)) * 100 + '%';
-        progressSuccess.style.width = percent;
+        updateSteps();
     }
-    updateSteps();
+    getSteps();
 }
 
-
-
-getSteps();
