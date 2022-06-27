6d73a5726f0cc6d5b4693b3df6c36b559a972806
Сергей Субботин <zarodiny@yandex.ru>
2022-06-27 13:29:41 +1000

    меняет логику отображения скрытых айтемов на странице портфолио

--- assets/js/load-more.js
+++ assets/js/load-more.js
@@ -1,59 +1,18 @@
-const portfolioItems = document.querySelectorAll('.portfolio__item');
-const portfolioItemsVisible = document.querySelectorAll('.portfolio__item--visible');
-const loadMoreBtn = document.querySelector('.load-more__btn');
+const loadMoreBtn = document.querySelector('.load-more-js');
+const portfolioListLength = document.querySelectorAll('.portfolio__item').length;
 
-
-
-// Удаляет лишние portfolio__item--visible со страницы которых больше 6
-const hideMoreItems = (selector) => {
-    if (selector.length > 6) {
-        const arr = Array.from(selector);
-        //создает массив из 6 элементов (с 6 по portfolioItemsVisible.lenhgt)
-        const hiddenItems = arr.slice(6, selector.length);
-
-        //удаляет с  лишних класс portfolio__item--visible
-        hiddenItems.forEach(el => {
-            el.classList.remove('portfolio__item--visible');
-        });
-    }
-};
-
-hideMoreItems(portfolioItemsVisible);
-
-
-
-//если длина portfolio__item--visible  больше 8 удаляет кнопку loadMoreBtn
 if (loadMoreBtn) {
+    let items = 6;
+
     loadMoreBtn.addEventListener('click', () => {
-        portfolioItems.forEach(el => {
-            el.classList.remove('portfolio__item--visible');
-            el.style.display = 'block';
-        });
-        if (portfolioItemsVisible.length <= 8) {
-            loadMoreBtn.style.display = 'inline-flex';
-        } else {
-            loadMoreBtn.style.display = 'none';
+        items += 3;
+        const array = Array.from(document.querySelector('.portfolio__list').children);
+        const visibleItem = array.slice(0, items);
+
+        visibleItem.forEach(item => item.classList.add('is-visible'));
+
+        if (visibleItem.length === portfolioListLength) {
+            loadMoreBtn.style.display = "none";
         }
-    }, { once: true });
+    });
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// console.log(portfolioItems);
